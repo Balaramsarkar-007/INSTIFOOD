6eae6f7fa315a39a8bd3f57e917cfabb11c49220
Balaram Sarkar <balaramsarkar.ramnagar@gmail.com>
2025-07-10 12:32:10 +0530

    Updates files

--- backend/index.js
+++ backend/index.js
@@ -30,7 +30,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 // MongoDB connection
-const mongo_url = 'mongodb://127.0.0.1:27017/instifood';
+const mongo_url = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/instifood';
 main()
 .then( () => {
     console.log("connection is sucessfull");
